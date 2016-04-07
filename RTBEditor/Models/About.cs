8a91ac41c4a88fc180a46bf1589c2adc7b1e6203
sourabh somani <sourabh_somani2010@hotmail.com>
2016-04-07 11:27:10 +0530

    RichTextBox Editor

---
+++ RTBEditor/Models/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace RTBEditor.Models
+{
+    public class About
+    {
+        [Required]
+        public int UserID { get; set; }
+
+        [AllowHtml]
+        [UIHint("tinymce_full_compressed")]
+        [Display(Name = "About")]
+        public string AboutMe { get; set; }
+    }
+}
---
+++ RTBEditor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RTBEditor.Startup))]
+namespace RTBEditor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
